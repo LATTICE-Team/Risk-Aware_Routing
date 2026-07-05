af6f25781bbd63b2f76b20884df01771b77c7cae
BenjaminHerrn <benjamin.herrn@tu-ilmenau.de>
2026-07-06 00:38:59 +0200

    Bugfixes und vollständige Implementierung CVRPTW
    zeitliche Nebenbedingungen wurden nicht korrekt mit "model +=" hinzugefügt. Das wurde behoben. Es wurde das CVRPTW für die Container vervollständigt. Die Roboter wurden nicht betrachtet.

--- vrp2.py
+++ vrp2.py
@@ -29,10 +29,11 @@ c = [[ model.add_var(var_type=mip.BINARY) for i in I] for k in K]               
 x = [[[ model.add_var(var_type=mip.BINARY) for j in I ] for i in I] for k in K]     # Container k goes from i to j
 r_0 = [[[ model.add_var(var_type=mip.BINARY) for j in I] for i in I] for v in V]    # Roboter v drives empty from i to j
 r_1 = [[[ model.add_var(var_type=mip.BINARY) for j in I] for i in I] for v in V]    # Roboter v drives with a Container from i to j
-robo_arrival = [[model.add_var(var_type=mip.CONTINUOUS) for i in I] for v in V]     # Arrival-Time Roboter v at Job i
-robo_departure = [[model.add_var(var_type=mip.CONTINUOUS) for i in I] for v in V]   # Departure-Time Roboter v at Job i
-cont_arrival = [[model.add_var(var_type=mip.CONTINUOUS) for i in I] for k in K]     # Arrival-Time Container k at Job i
-cont_departure = [[model.add_var(var_type=mip.CONTINUOUS) for i in I] for k in K]   # Departure-Time Container k at Job i
+robo_arrival = [[model.add_var(lb=0.0, var_type=mip.CONTINUOUS) for i in I] for v in V]     # Arrival-Time Roboter v at Job i
+robo_departure = [[model.add_var(lb=0.0, var_type=mip.CONTINUOUS) for i in I] for v in V]   # Departure-Time Roboter v at Job i
+cont_arrival = [[model.add_var(lb=0.0, var_type=mip.CONTINUOUS) for i in I] for k in K]     # Arrival-Time Container k at Job i
+cont_departure = [[model.add_var(lb=0.0, var_type=mip.CONTINUOUS) for i in I] for k in K]   # Departure-Time Container k at Job i
+container_return = [model.add_var(lb=0.0, var_type=mip.CONTINUOUS) for k in K]      # Rückkehrzeit Container zu Depot
 
 container_used = [model.add_var(var_type=mip.BINARY) for k in K]     # Container k is used
 roboter_used = [model.add_var(var_type=mip.BINARY) for v in V]     # Roboter v is used
@@ -41,18 +42,25 @@ model.objective = (mip.xsum(mip.xsum(mip.xsum(x[k][i][j]*w[i][j] for i in I)for 
                   + mip.xsum(mip.xsum(mip.xsum(r_0[v][i][j]*w[i][j] for i in I) for j in I) for v in V))
 
 #-----Constraints---------------------------------------------------------------------------------------
+# Kapazitätsbeschränkung Container
 for k in K:
-    model += mip.xsum(c[k][i] for i in I-{0})/n <= container_used[k]
-    model += mip.xsum(c[k][i] for i in I-{0}) <= C[k]
+    model += mip.xsum(d[i] * c[k][i] for i in I - {0}) <= C[k] * container_used[k]
 
-# wenn Container Job j macht, fährt er nach j
+# Container darf nur bewegt werden, wenn er auch benutzt wird
 for k in K:
-    for j in I-{0}:
-        model += mip.xsum(x[k][i][j] for i in I) == c[k][j]
+    for i in I - {0}:
+        model += c[k][i] <= container_used[k]
+
+# genau dann, wenn Container Job j macht, hat er eine eingehende und ausgehende Kante zu j
+for k in K:
+    for i in I-{0}:
+        model += mip.xsum(x[k][j][i] for j in I if j != i) == c[k][i]
+        model += mip.xsum(x[k][i][j] for j in I if j != i) == c[k][i]
 
 # jeder Job wird von genau einem Container erledigt
 for i in I-{0}:
     model += mip.xsum(c[k][i] for k in K) == 1
+
 
 # jeder Knoten wird genau von einem Container besucht
 for i in I-{0}:
@@ -61,43 +69,46 @@ for i in I-{0}:
 
 # jeder Container, der benutzt wird, startet und endet am Depot
 for k in K:
-    model += mip.xsum(x[k][0][i] for i in I) == container_used[k]
-    model += mip.xsum(x[k][i][0] for i in I) == container_used[k]
-for v in V:
-    model += mip.xsum(r_0[v][0][i]+r_1[v][0][i] for i  in I-{0}) == roboter_used[v]
-    model += mip.xsum(r_0[v][i][0]+r_1[v][i][0] for i  in I-{0}) == roboter_used[v]
+    model += mip.xsum(x[k][0][j] for j in I if j != 0) == container_used[k]
+    model += mip.xsum(x[k][i][0] for i in I if i != 0) == container_used[k]
+
 
 # eingehend = ausgehend
 for k in K:
     for j in I:
         model += mip.xsum(x[k][i][j] for i in I) - mip.xsum(x[k][j][i] for i in I) == 0
-for v in V:
-    for j in I:
-        model += mip.xsum(r_0[v][i][j]+r_1[v][i][j] for i in I) - mip.xsum(r_0[v][j][i]+r_1[v][j][i] for i in I) == 0
+
 
 # Keine Verbindung von Knoten zu sich selbst
 for i in I:
     for k in K:
         model += x[k][i][i] == 0    
-    for v in V:
-        model += r_0[v][i][i] == 0
-        model += r_1[v][i][i] == 0
 
-# Kapazitätsbeschränkung Container
-for k in K:
-    model += mip.xsum(mip.xsum(x[k][i][j]*d[j] for i in I-{0}) for j in I-{0}) <= C[k]
 
 # zeitliche Constraints Container
 for k in K:
     for i in I-{0}:
-        cont_arrival[k][i] + service_duration[i] <= cont_departure[k][i]
-        cont_arrival[k][i] <= time_window[i][1]
-        cont_arrival[k][i] >= time_window[i][0]
-for k in K:
-    for i in I:
-        for j in I:
-            T[i][j] - M*(1-x[k][i][j]) <= cont_arrival[k][j] - cont_departure[k][i]
+        model += cont_departure[k][i] >= cont_arrival[k][i] + service_duration[i] - M * (1 - c[k][i])
+        model += cont_departure[k][i] <= cont_arrival[k][i] + service_duration[i] + M * (1 - c[k][i])
+        model += cont_arrival[k][i] <= time_window[i][1] * c[k][i]
+        model += cont_arrival[k][i] >= time_window[i][0] * c[k][i]
 
+# zeitliche Konsistenz von Depot zu Kunden
+for k in K:
+    for j in I - {0}:
+        model += cont_arrival[k][j] >= T[0][j] - M*(1 - x[k][0][j])
+
+# zeitliche Konsistenz Kunde zu Kunde
+for k in K:
+    for i in I - {0}:
+        for j in I - {0}:
+            if i != j:
+                model += cont_arrival[k][j] >= cont_arrival[k][i] + service_duration[i] + T[i][j] - M*(1 - x[k][i][j])
+
+# zeitliche Konsistenz Containerrückkehr
+for k in K:
+    for i in I - {0}:
+        model += container_return[k] >= cont_arrival[k][i] + service_duration[i] + T[i][0] - M * (1 - x[k][i][0])
 
 #-----Solution---------------------------------------------------------------------------------------
 status = model.optimize(max_seconds=300)
